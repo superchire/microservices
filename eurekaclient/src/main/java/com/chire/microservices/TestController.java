7afd486c8590e3430e3c407fc139e2e198bf8ba9
superchire <chire1205@gmail.com>
2019-08-16 15:41:04 +0800

    更新 1.ribbon 2.service provider ha 3.config server 4.config server ha

--- eurekaclient/src/main/java/com/chire/microservices/TestController.java
+++ eurekaclient/src/main/java/com/chire/microservices/TestController.java
@@ -1,13 +1,25 @@
 package com.chire.microservices;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
+    @Value("${chire.hello}")
+    private String hello;
+
+
     @GetMapping("test")
     public String test() {
         return "test_0";
+    }
+
+
+
+    @GetMapping("hello")
+    public String from() {
+        return this.hello;
     }
 }
